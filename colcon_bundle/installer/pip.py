bb5f54c71eeaaff3eeb4064c9f78659de0afa856
r-meagher <r-meagher@users.noreply.github.com>
2018-10-30 15:52:18 -0700

    Add pip support for colcon bundle (#4)
    * Add pip support for colcon bundle
    
    * Update to use python2
    
    Co-Authored-By: r-meagher <r-meagher@users.noreply.github.com>

---
+++ colcon_bundle/installer/pip.py
@@ -0,0 +1,69 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import subprocess
+
+from colcon_bundle.installer import BundleInstallerExtensionPoint
+from colcon_bundle.verb import logger
+
+
+class PipBundleInstallerExtensionPoint(BundleInstallerExtensionPoint):
+    """Python 2 pip installer."""
+
+    __slots__ = (
+        'context',
+        '_packages',
+        '_cache_path'
+    )
+
+    PRIORITY = 10
+
+    def __init__(self):  # noqa: D107
+        self.context = None
+        self._packages = []
+        self._cache_path = None
+
+    def add_arguments(self, *, parser):  # noqa: D102
+        parser.add_argument(
+            '--pip-argument', default='build',
+            help='The base path for all build directories (default: build)')
+
+    def initialize(self, context):  # noqa: D102
+        self.context = context
+        self._cache_path = context.cache_path
+
+    def add_to_install_list(self, name, metadata=None):
+        """
+        Mark the python package for installation.
+
+        :param name: Name of the python package to install. This supports
+        PEP440 compliant version specifiers.
+        :return: None
+        """
+        self._packages.append(name)
+
+    def remove_from_install_list(self, name, metadata=None):  # noqa: D102
+        self._packages.remove(name)
+
+    def install(self):  # noqa: D102
+        if len(self._packages) == 0:
+            logger.info('No pip dependencies to install.')
+            return {'message': 'No dependencies installed...'}
+
+        logger.info('Installing pip dependencies...')
+        python_path = os.path.join(self.context.prefix_path, 'usr', 'bin',
+                                   'python2')
+        subprocess.check_call(
+            [python_path, '-m', 'pip', 'install', '-U', 'pip', 'setuptools'])
+
+        requirements = os.path.join(self._cache_path, 'requirements')
+        with open(requirements, 'w') as req:
+            for name in self._packages:
+                req.write(name.strip() + '\n')
+
+        subprocess.check_call(
+            [python_path, '-m', 'pip', 'install', '--ignore-installed',
+             '-r', requirements])
+
+        return {'requirements': self._packages}
