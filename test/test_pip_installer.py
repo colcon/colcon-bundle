16b12975404b2d8ab6f3e71cdee0ce15b9ce8e64
Matthew Murphy <matmur@amazon.com>
2018-12-17 14:48:46 -0800

    Update pip installer to not install if dependencies didn't change

--- test/test_pip_installer.py
+++ test/test_pip_installer.py
@@ -113,3 +113,58 @@ def test_install_with_additional_arguments(check_output, check_call):
     finally:
         shutil.rmtree(cache_dir)
         shutil.rmtree(prefix)
+
+@patch('subprocess.check_call')
+@patch('subprocess.check_output')
+def test_install_not_required(check_output, check_call):
+    installer = PipBundleInstallerExtensionPoint()
+    cache_dir = mkdtemp()
+
+    prefix = mkdtemp()
+    python_path = os.path.join(prefix, 'usr', 'bin', 'python2')
+    context_args = Mock()
+    context_args.pip_args = []
+    context = BundleInstallerContext(
+        args=context_args, cache_path=cache_dir, prefix_path=prefix)
+    check_output.return_value = 'pkg1==3.4.5\npkg2==3.1.2\n'
+    try:
+        installer.initialize(context)
+        installer.add_to_install_list('pkg1==3.4.5')
+        installer.add_to_install_list('pkg2>=3.1.2')
+        installer.add_to_install_list('remove_me')
+        installer.remove_from_install_list('remove_me')
+        result = installer.install()
+
+        args_list = check_call.call_args_list
+        args = args_list[0][0][0]
+        assert args[0] == python_path
+        # Ensure we upgrade pip/setuptools
+        assert args[1:] == ['-m', 'pip', 'install', '-U', 'pip', 'setuptools']
+
+        args = args_list[1][0][0]
+        assert args[0] == python_path
+        assert args[1:-1] == [
+            '-m', 'pip', 'install', '--ignore-installed', '-r']
+
+        assert result == {
+            'installed_packages': [
+                {
+                    'name': 'pkg1',
+                    'version': '3.4.5'
+                },
+                {
+                    'name': 'pkg2',
+                    'version': '3.1.2'
+                }
+            ]
+        }
+
+        result_2 = installer.install()
+        assert result == result_2
+        # Verify we haven't called pip
+        assert check_call.call_count == 3
+        # Verify we haven't called pip freeze
+        assert check_output.call_count == 1
+    finally:
+        shutil.rmtree(cache_dir)
+        shutil.rmtree(prefix)
