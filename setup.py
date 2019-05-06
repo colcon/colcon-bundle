98d96765e2d4b71f775fdc29a31633df36515b44
Matthew Murphy <matmur@amazon.com>
2019-05-05 21:04:57 -0700

    Remove setuptools upgrade from integration tests

--- setup.py
+++ setup.py
@@ -16,8 +16,7 @@ setup(
     author_email='matmur@amazon.com',
     maintainer='Matthew Murphy',
     maintainer_email='matmur@amazon.com',
-    Changelog='https://github.com/colcon/colcon-bundle/milestones?direction=desc&sort=due_date&state=closed',
-    GitHub='https://github.com/colcon/colcon-bundle/',
+    url='https://github.com/colcon/colcon-bundle/',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
@@ -40,21 +39,7 @@ setup(
         'colcon-python-setup-py>=0.2.1',
         'distro>=1.3.0'
     ],
-    packages=find_packages(exclude=['test']),
-    entry_points={
-        'colcon_core.verb': [
-            'bundle = colcon_bundle.verb.bundle:BundleVerb'
-        ],
-        'colcon_bundle.installer': [
-            'apt = colcon_bundle.installer.apt:AptBundleInstallerExtension',
-            'pip = colcon_bundle.installer.pip:PipBundleInstallerExtensionPoint',
-            'pip3 = colcon_bundle.installer.pip3:Pip3BundleInstallerExtensionPoint'
-        ],
-        'colcon_bundle.task.bundle': [
-            'python = colcon_bundle.task.python.bundle:PythonBundleTask'
-        ]
-    },
-    tests_require = [
+    tests_require=[
         'flake8',
         'flake8-blind-except',
         'flake8-builtins',
@@ -71,7 +56,22 @@ setup(
         'pytest',
         'pytest-cov',
         'pytest-asyncio',
+        'pycodestyle==2.3.0',
     ],
     zip_safe=True,
-    include_package_data=True
+    include_package_data=True,
+    packages=find_packages(exclude=['test', 'test.*', 'integration']),
+    entry_points={
+        'colcon_core.verb': [
+            'bundle = colcon_bundle.verb.bundle:BundleVerb'
+        ],
+        'colcon_bundle.installer': [
+            'apt = colcon_bundle.installer.apt:AptBundleInstallerExtension',
+            'pip = colcon_bundle.installer.pip:PipBundleInstallerExtensionPoint',
+            'pip3 = colcon_bundle.installer.pip3:Pip3BundleInstallerExtensionPoint'
+        ],
+        'colcon_bundle.task.bundle': [
+            'python = colcon_bundle.task.python.bundle:PythonBundleTask'
+        ]
+    }
 )
