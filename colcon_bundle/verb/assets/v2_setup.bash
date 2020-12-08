dc4ae2b56fc5a891ca227e9b6399871cc4f5f962
Anurag Madan <madaanur@amazon.com>
2020-12-08 17:21:11 +0000

    try to fix exports 1/?

--- colcon_bundle/verb/assets/v2_setup.bash
+++ colcon_bundle/verb/assets/v2_setup.bash
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-export PATH="usr/sbin:usr/bin:usr/local/bin:sbin:bin:$PATH"
-export LD_LIBRARY_PATH="lib:usr/lib:lib/x86_64-linux-gnu:usr/lib/x86_64-linux-gnu:usr/lib/x86_64-linux-gnu/mesa:$LD_LIBRARY_PATH:lib/arm-linux-gnueabihf:usr/lib/arm-linux-gnueabihf:$LD_LIBRARY_PATH:lib/aarch64-linux-gnu:usr/lib/aarch64-linux-gnu"
+# export PATH="usr/sbin:usr/bin:usr/local/bin:sbin:bin:$PATH"
+# export LD_LIBRARY_PATH="lib:usr/lib:lib/x86_64-linux-gnu:usr/lib/x86_64-linux-gnu:usr/lib/x86_64-linux-gnu/mesa:$LD_LIBRARY_PATH:lib/arm-linux-gnueabihf:usr/lib/arm-linux-gnueabihf:$LD_LIBRARY_PATH:lib/aarch64-linux-gnu:usr/lib/aarch64-linux-gnu"
 
 DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd)"
+
+
+export PATH="$DIR/usr/sbin:$DIR/usr/bin:$DIR/usr/local/bin:$DIR/sbin:$DIR/bin:$PATH"
+export LD_LIBRARY_PATH="$DIR/lib:$DIR/usr/lib:$DIR/lib/x86_64-linux-gnu:$DIR/usr/lib/x86_64-linux-gnu:$DIR/usr/lib/x86_64-linux-gnu/mesa:$LD_LIBRARY_PATH:$DIR/lib/arm-linux-gnueabihf:$DIR/usr/lib/arm-linux-gnueabihf:$LD_LIBRARY_PATH:$DIR/lib/aarch64-linux-gnu:$DIR/usr/lib/aarch64-linux-gnu"
+
+
 
 export LC_ALL=C.UTF-8
 
