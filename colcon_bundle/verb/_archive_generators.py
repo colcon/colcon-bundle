dc965cdd72e5ba009524398ead563bc70c2dd628
Scott K Logan <logans@cottsay.net>
2020-12-15 16:43:21 -0800

    Explicitly fix script permissions after copying
    There are two reasons for this change:
    1. The permissions on these files may get changed if the source code is
       packaged into a format that doesn't support storing the permissions,
       and the current implementation relies on copying the files with the
       permissions intact.
    2. Some of these scripts are templates, which can't be executed as-is.
       Removing the executable permissions from the file makes this more
       clear.

--- colcon_bundle/verb/_archive_generators.py
+++ colcon_bundle/verb/_archive_generators.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import stat
 import tarfile
 from typing import List
 
@@ -9,6 +10,9 @@ from colcon_bundle.verb._overlay_utilities import \
     create_dependencies_overlay, create_workspace_overlay
 from colcon_bundle.verb._path_context import PathContext
 from colcon_bundle.verb.bundlefile import Bundle
+
+
+_ALL_X = stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
 
 
 def generate_archive_v1(path_context):
@@ -32,6 +36,7 @@ def generate_archive_v1(path_context):
         os.path.dirname(os.path.realpath(__file__)), 'assets')
     shellscript_path = os.path.join(assets_directory, 'v1_setup.sh')
     shutil.copy2(shellscript_path, os.path.join(staging_path, 'setup.sh'))
+    os.chmod(os.path.join(staging_path, 'setup.sh'), _ALL_X)
     bundle_workspace_install_path = os.path.join(
         staging_path, 'opt', 'install')
     if os.path.exists(bundle_workspace_install_path):
