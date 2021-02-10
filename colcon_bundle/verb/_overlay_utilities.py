6edc4c83071ccc2c246a9e256e59cfd1703dbae4
Jesse Ikawa <jikawa@amazon.com>
2021-02-10 15:40:17 -0800

    more paths cleanup
    Signed-off-by: Jesse Ikawa <jikawa@amazon.com>

--- colcon_bundle/verb/_overlay_utilities.py
+++ colcon_bundle/verb/_overlay_utilities.py
@@ -18,34 +18,33 @@ _CONTEXT_VAR_SH = {'shell': 'sh'}
 
 
 def create_workspace_overlay(install_base: str,
-                             ws_staging_path: str,
+                             workspace_staging_path: str,
                              overlay_path: str):
     """
     Create overlay from user's built workspace install directory.
 
     :param str install_base: Path to built workspace install directory
-    :param str ws_staging_path: Path to stage the overlay build at
+    :param str workspace_staging_path: Path to stage the overlay build at
     :param str overlay_path: Name of the overlay file (.tar.gz)
     """
-    ws_install_path = Path(ws_staging_path) / 'opt' / 'built_workspace'
+    ws_install_path = Path(workspace_staging_path) / 'opt' / 'built_workspace'
 
-    shutil.rmtree(ws_staging_path, ignore_errors=True)
-
-    shellscript_dest = Path(ws_staging_path) / 'setup.sh'
-    shellscript_dest_bash = Path(ws_staging_path) / 'setup.bash'
+    shutil.rmtree(workspace_staging_path, ignore_errors=True)
 
     # install_base: Directory with built artifacts from the workspace
-    os.mkdir(ws_staging_path)
+    os.mkdir(workspace_staging_path)
 
-    _rendering_template(
-        'v2_workspace_setup.jinja2.sh',
+    shellscript_dest = Path(workspace_staging_path) / 'setup.sh'
+    _render_template(
+        Path('v2_workspace_setup.jinja2.sh'),
         shellscript_dest,
         _CONTEXT_VAR_SH
     )
     shellscript_dest.chmod(0o755)
 
-    _rendering_template(
-        'v2_workspace_setup.jinja2.sh',
+    shellscript_dest_bash = Path(workspace_staging_path) / 'setup.bash'
+    _render_template(
+        Path('v2_workspace_setup.jinja2.sh'),
         shellscript_dest_bash,
         _CONTEXT_VAR_BASH
     )
@@ -55,9 +54,9 @@ def create_workspace_overlay(install_base: str,
 
     # This is required because python3 shell scripts use a hard
     # coded shebang
-    update_shebang(ws_staging_path)
+    update_shebang(workspace_staging_path)
 
-    recursive_tar_gz_in_path(overlay_path, ws_staging_path)
+    recursive_tar_gz_in_path(Path(overlay_path), Path(workspace_staging_path))
 
 
 def create_dependencies_overlay(staging_path: str, overlay_path: str):
@@ -76,17 +75,16 @@ def create_dependencies_overlay(staging_path: str, overlay_path: str):
     ))
 
     shellscript_dest = Path(dep_staging_path) / 'setup.sh'
-    shellscript_dest_bash = Path(dep_staging_path) / 'setup.bash'
-
-    _rendering_template(
-        'v2_setup.jinja2.sh',
+    _render_template(
+        Path('v2_setup.jinja2.sh'),
         shellscript_dest,
         _CONTEXT_VAR_SH
     )
     shellscript_dest.chmod(0o755)
 
-    _rendering_template(
-        'v2_setup.jinja2.sh',
+    shellscript_dest_bash = Path(dep_staging_path) / 'setup.bash'
+    _render_template(
+        Path('v2_setup.jinja2.sh'),
         shellscript_dest_bash,
         _CONTEXT_VAR_BASH
     )
@@ -94,31 +92,29 @@ def create_dependencies_overlay(staging_path: str, overlay_path: str):
 
     if dep_tar_gz_path.exists():
         dep_tar_gz_path.unlink()
-    recursive_tar_gz_in_path(str(dep_tar_gz_path), str(dep_staging_path))
+    recursive_tar_gz_in_path(dep_tar_gz_path, dep_staging_path)
 
 
-def recursive_tar_gz_in_path(output_path: str, path: str):
+def recursive_tar_gz_in_path(output_path: Path, tar_path: Path):
     """
     Create a tar.gz archive of all files inside a directory.
 
     This function includes all sub-folders of path in the root of the tarfile
 
     :param output_path: Name of archive file to create
-    :param path: path to recursively collect all files and include in
+    :param tar_path: path to recursively collect all files and include in
     tar.gz. These will be included with path as the root of the archive.
     """
-    tar_path = Path(path)
-    with tarfile.open(output_path, mode='w:gz', compresslevel=5) as tar:
+    with tarfile.open(str(output_path), mode='w:gz', compresslevel=5) as tar:
         logger.info(
-            'Creating tar of {path}'.format(path=str(tar_path)))
-        for name in tar_path.iterdir():
-            some_path = Path(tar_path) / name
-            tar.add(str(some_path), arcname=os.path.basename(str(some_path)))
+            'Creating tar of {path}'.format(path=tar_path))
+        for child in tar_path.iterdir():
+            tar.add(str(child), arcname=str(child.name))
 
 
-def _rendering_template(template_name: str,
-                        script_dest: Path,
-                        context_vars: dict):
+def _render_template(template_name: Path,
+                     script_dest: Path,
+                     context_vars: dict):
     """
     Render setup.bash or setup.sh files from template.
 
@@ -129,13 +125,13 @@ def _rendering_template(template_name: str,
     :param context_vars: dictionary of values to be used for the variables in
     the template
     """
-    template_location = Path(__file__).parent.absolute() / 'assets/'
+    src = Path(__file__).parent.absolute() / 'assets' / template_name
     env = Environment(
         autoescape=select_autoescape(['html', 'xml']),
-        loader=FileSystemLoader(str(template_location)),
+        loader=FileSystemLoader(str(src.parent)),
         keep_trailing_newline=True,
     )
-    template = env.get_template(template_name)
+    template = env.get_template(str(src.name))
 
     with script_dest.open('w') as file:
         file.write(template.render(context_vars))
